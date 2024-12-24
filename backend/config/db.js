6663240c044644d863d625c569de46a8f91befea
webdev9596 <stlens2605@gmail.com>
2024-12-25 05:07:15 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,20 @@
 import mongoose from "mongoose"
 
 const connectDb = async () => {
-    await mongoose.connect(process.env.DB_URL)
-}
+  try {
+    const uri = process.env.DB_URL;
+    if (!uri) {
+      throw new Error('MONGO_URI is not defined');
+    }
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
 
 export default connectDb
